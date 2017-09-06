bf6d460f241031345ee24bce2584b38667de2b48
etoxin <adam@etoxin.net>
2017-09-07 00:18:23 +1000

    Added dockerfile

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -51,7 +51,7 @@ angular.module('synthApp', [])
 
         // that's the prefixing sorted.
         // window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        var synth = new webkitAudioContext();
+        var synth = new AudioContext();
         for (var i = 0; i < $scope.amountOfoscillators; i++) {
             $scope.oscillators[i] = synth.createOscillator();
             $scope.oscillators[i].frequency.value = $scope.frequency - ($scope.detune * i);
